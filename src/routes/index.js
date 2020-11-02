4eeae452d327a09332f882f0aab859914cdc93ca
Martins Obayomi <obayomimartins96@gmail.com>
2020-11-02 08:07:43 +0100

    Fix failing tests

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 
-import bookRouter from './book';
 import { jsonResponse } from '../utils/helpers';
 
 const { sendSuccess } = jsonResponse;
@@ -10,6 +9,5 @@ const routes = Router();
 routes.get('/', (req, res) =>
   sendSuccess(res, 200, 'Welcome to Books API version 1'),
 );
-routes.use('/books', bookRouter);
 
 export default routes;
